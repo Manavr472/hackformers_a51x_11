7ec58ab6a23393939df6c85f4fd2a0bf3ff31322
Manavr472 <manavr472@gmail.com>
2023-03-19 04:18:48 +0530

    gg

--- boot/main.js
+++ boot/main.js
@@ -30,7 +30,6 @@ class MyHeader extends HTMLElement{
                   <li><a class="dropdown-item" href="pdf.html">Pdfs</a></li>
                 </ul>
               <li class="nav-item">
-              
                 <a class="nav-link active" aria-current="page" href="contact.html">Contact Us</a>
               </li>
             </ul>
